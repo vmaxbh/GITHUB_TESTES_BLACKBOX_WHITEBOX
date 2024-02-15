df7cd43dcd9403d13000d11372db7ae0a6187f26
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-02-15 16:54:52 -0300

    :build: < Teste de config de pipeline >

--- cypress/support/Test Black Box/login.js
+++ cypress/support/Test Black Box/login.js
@@ -1,8 +1,8 @@
 Cypress.Commands.add('login', (username = 'USERNAME', password = 'PASSWORD') => {
     cy.visit('/').log('URl acessada!')
     cy.get('div.width-lg-auto > :nth-child(2) > .d-inline-block').click().log('Botão para acesso a pagina de Login clicado!')
-    cy.get('#login_field').should('be.visible').type(Cypress.env(username)).log('Usuário incluído no campo de username!')
-    cy.get('#password').should('be.visible').type(Cypress.env(password)).log('Senha Incluída no campo de password!')
+    cy.get('#login_field', { timeout:  10000 }).should('be.visible').type(Cypress.env(username)).log('Usuário incluído no campo de username!')
+    cy.get('#password', { timeout:  10000 }).should('be.visible').type(Cypress.env(password)).log('Senha Incluída no campo de password!')
     cy.get('.position-relative > .btn').click().log('Botão de acessar Clicado!')
 });
 
