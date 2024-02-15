bdd459a6d38cea44786e8057d1153fa6abe0e210
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-02-15 15:05:30 -0300

    :build: < Teste de config de pipeline >

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 const dateTime = moment().format('DD-MM-YYYY_HH-mm-ss');
 
 module.exports = defineConfig({
+  projectId: "pexjpr",
   chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://github.com/',
