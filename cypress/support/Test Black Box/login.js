6681ac5c4e806dc4ec1c1e14876d9be209ccad34
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-02-15 17:13:54 -0300

    :build: < Teste de config de pipeline >

--- cypress/support/Test Black Box/login.js
+++ cypress/support/Test Black Box/login.js
@@ -1,9 +1,10 @@
 Cypress.Commands.add('login', (username = 'USERNAME', password = 'PASSWORD') => {
-    cy.visit('/').log('URl acessada!')
-    cy.get('div.width-lg-auto > :nth-child(2) > .d-inline-block', { timeout:  50000 }).click().log('Botão para acesso a pagina de Login clicado!')
-    cy.get('#login_field', { timeout:  50000 }).should('be.visible').type(Cypress.env(username)).log('Usuário incluído no campo de username!')
-    cy.get('#password', { timeout:  50000 }).should('be.visible').type(Cypress.env(password)).log('Senha Incluída no campo de password!')
-    cy.get('.position-relative > .btn').click().log('Botão de acessar Clicado!')
+    cy.visit('/').log('URL acessada!');
+    cy.get('div.width-lg-auto > :nth-child(2) > .d-inline-block').click().log('Botão para acesso à página de Login clicado!');
+    cy.get('#login_field', { timeout:   20000 }).should('be.visible').type(Cypress.env(username)).log('Usuário incluído no campo de username!');
+    cy.get('#password', { timeout:   20000 }).should('be.visible').type(Cypress.env(password)).log('Senha incluída no campo de password!');
+    cy.get('.position-relative > .btn').click().log('Botão de acessar clicado!');
+  
 });
 
 Cypress.Commands.add('loginUsuárioInválido', (username = 'USERNAME', password = 'PASSWORD') => {
