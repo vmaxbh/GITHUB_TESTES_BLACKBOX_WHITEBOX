ebc37701c5a62813b5ef122d52166d47e5343472
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-02-16 11:16:35 -0300

    :build: < Teste de config de pipeline >

--- cypress/support/Test Black Box/login.js
+++ cypress/support/Test Black Box/login.js
@@ -4,7 +4,7 @@ Cypress.Commands.add('login', (username = 'USERNAME', password = 'PASSWORD') => 
     cy.get('#login_field').should('be.visible').type(Cypress.env(username)).should('have.value', Cypress.env(username)).log('Usuário incluído no campo de username!');
     cy.get('#password').should('be.visible').type(Cypress.env(password)).should('have.value', Cypress.env(password)).log('Senha incluída no campo de password!');
     cy.get('.position-relative > .btn').click().log('Botão de acessar clicado!');
-    cy.wait(1000); // Aumente o tempo de espera para   1000ms para dar tempo suficiente para a página processar
+    cy.wait(5000); // Aumente o tempo de espera para   1000ms para dar tempo suficiente para a página processar
     cy.get('.AppHeader-context-item-label', { timeout:   10000 }).should('contain', 'Dashboard');
 });
 
