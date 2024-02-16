e52dda63953e7dffb489299d981282ad4076e389
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-02-16 11:14:42 -0300

    :build: < Teste de config de pipeline >

--- cypress/support/Test Black Box/login.js
+++ cypress/support/Test Black Box/login.js
@@ -1,38 +1,11 @@
-Cypress.Commands.add('login', (username = 'USERNAME', password = 'PASSWORD', retries =  1) => {
-    const login = () => {
-        cy.visit('/').log('URL acessada!');
-        cy.get('div.width-lg-auto > :nth-child(2) > .d-inline-block').click().log('Botão para acesso à página de Login clicado!');
-        cy.get('#login_field').should('be.visible').type(Cypress.env(username)).should('have.value', Cypress.env(username)).log('Usuário incluído no campo de username!');
-        cy.get('#password').should('be.visible').type(Cypress.env(password)).should('have.value', Cypress.env(password)).log('Senha incluída no campo de password!');
-        cy.get('.position-relative > .btn').click().log('Botão de acessar clicado!');
-        cy.wait(10)
-        cy.get('.AppHeader-context-item-label', { timeout: 10000 }).should('contain', 'Dashboard')
-    };
-
-    const checkLoginSuccess = () => {
-        // Verifica se a URL atual inclui '/dashboard', indicando que o usuário está logado.
-        return cy.url().then(url => {
-            if (url.includes('/dashboard')) {
-                // Se a URL incluir '/dashboard', retorna true para indicar sucesso.
-                return true;
-            } else {
-                // Se a URL não incluir '/dashboard', retorna false para indicar falha.
-                return false;
-            }
-        });
-    };
-
-    const attemptLogin = (attempt) => {
-        login();
-        cy.then(() => {
-            if (!checkLoginSuccess() && attempt < retries) {
-                cy.log(`Falha no login, tentando novamente... (tentativa ${attempt +  1} de ${retries})`);
-                attemptLogin(attempt +  1);
-            }
-        });
-    };
-
-    attemptLogin(0);
+Cypress.Commands.add('login', (username = 'USERNAME', password = 'PASSWORD') => {
+    cy.visit('/').log('URL acessada!');
+    cy.get('div.width-lg-auto > :nth-child(2) > .d-inline-block').click().log('Botão para acesso à página de Login clicado!');
+    cy.get('#login_field').should('be.visible').type(Cypress.env(username)).should('have.value', Cypress.env(username)).log('Usuário incluído no campo de username!');
+    cy.get('#password').should('be.visible').type(Cypress.env(password)).should('have.value', Cypress.env(password)).log('Senha incluída no campo de password!');
+    cy.get('.position-relative > .btn').click().log('Botão de acessar clicado!');
+    cy.wait(1000); // Aumente o tempo de espera para   1000ms para dar tempo suficiente para a página processar
+    cy.get('.AppHeader-context-item-label', { timeout:   10000 }).should('contain', 'Dashboard');
 });
 
 Cypress.Commands.add('loginUsuárioInválido', (username = 'USERNAME', password = 'PASSWORD') => {
